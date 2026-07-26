64940b59d7aa1ee274c248c87c76aea5ac4e18ea
AhmadKharfan <plamoplamo12@gmail.com>
2026-07-26 11:10:55 +0300

    drop the jgit test dependency from the app module.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -158,7 +158,6 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.okhttp.mockwebserver)
-    testImplementation(libs.jgit)
     testImplementation(platform(libs.koin.bom))
     testImplementation(libs.koin.test)
     androidTestImplementation(platform(libs.androidx.compose.bom))
