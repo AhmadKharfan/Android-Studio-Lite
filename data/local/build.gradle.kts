4b5dd6a95865fc3484894d68b03bb59cce44b847
AhmadKharfan <plamoplamo12@gmail.com>
2026-07-26 10:35:23 +0300

    move the local data tests into the local data module.

--- data/local/build.gradle.kts
+++ data/local/build.gradle.kts
@@ -6,4 +6,6 @@ dependencies {
     implementation(projects.data.templates)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.kotlinx.coroutines.android)
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
