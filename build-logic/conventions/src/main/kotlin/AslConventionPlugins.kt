8e7ddd1522fdc495686b4c03e8193be281adf5c2
AhmadKharfan <plamoplamo12@gmail.com>
2026-07-28 11:36:26 +0300

    chore(build): enable compose previews in feature modules

--- build-logic/conventions/src/main/kotlin/AslConventionPlugins.kt
+++ build-logic/conventions/src/main/kotlin/AslConventionPlugins.kt
@@ -1,6 +1,9 @@
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 class AslAndroidApplicationConventionPlugin : Plugin<Project> {
@@ -22,6 +25,17 @@ class AslAndroidComposeLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         pluginManager.apply("asl.android.library")
         pluginManager.apply("org.jetbrains.kotlin.plugin.compose")
+        configureComposePreviewTooling()
+    }
+}
+
+private fun Project.configureComposePreviewTooling() {
+    val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+    val bom = libs.findLibrary("androidx-compose-bom").get()
+    dependencies {
+        add("implementation", platform(bom))
+        add("implementation", libs.findLibrary("androidx-compose-ui-tooling-preview").get())
+        add("debugImplementation", libs.findLibrary("androidx-compose-ui-tooling").get())
     }
 }
 
